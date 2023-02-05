dcfd0b9bc340572782b2dc3df847ac6cba222b54
SmalBox <505414969@qq.com>
2023-02-05 13:58:58 +0800

    add: 添加版权页模板功能。导入音乐素材。添加背景音乐。

--- Assets/Scripts/UI/StartPanel/GameStartPanel.cs
+++ Assets/Scripts/UI/StartPanel/GameStartPanel.cs
@@ -5,6 +5,12 @@ public class GameStartPanel : UIBase
 {
     /// <summary> 开始按钮 </summary>
     public Button startBtn;
+
+    /// <summary> 版权按钮 </summary>
+    public Button copyrightBtn;
+
+    /// <summary> 版权内容 </summary>
+    public Button copyrightContentBtn;
     
     public override void Init()
     {
@@ -15,17 +21,24 @@ public class GameStartPanel : UIBase
     {
         base.Register();
         startBtn.onClick.AddListener(OnStartBtn);
+        copyrightBtn.onClick.AddListener(OnCopyrightBtn);
     }
 
     public override void UnRegister()
     {
         base.UnRegister();
         startBtn.onClick.RemoveAllListeners();
+        copyrightBtn.onClick.RemoveAllListeners();
     }
 
     private void OnStartBtn()
     {
         GameMain.Ins.StartGame();
         Destroy(gameObject);
+    }
+
+    private void OnCopyrightBtn()
+    {
+        copyrightContentBtn.gameObject.SetActive(true);
     }
 }
